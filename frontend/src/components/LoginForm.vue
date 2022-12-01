3f5b2d972c6909a458bd137bb633c1cec1efbd18
Robert Matsar <r.r.matsar@student.tudelft.nl>
2022-12-01 15:59:09 +0200

    Added login and signup basic components

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container-sm w-25 mt-5">
+    <form class="needs-validation">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control needs-validation" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control needs-validation" id="exampleInputPassword1">
+      </div>
+    </form>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <button type="submit" class="btn btn-secondary">Log in</button>
+        </div>
+        <div class="col">
+          <button type="button" class="btn btn-outline-secondary">Sign up</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Login from "../views/Login.vue"
+import Signup from "../views/Signup.vue"
+
+
+const routes = [
+{
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+},
+{
+  path: "/login",
+  name: "Login",
+  component: Login,
+}
+
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-
-  ],
-});
+  routes
+})
 
 export default router;
