2559e14a8ea515befc8b1e12e982fe31a6d0cb36
CoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-12-01 17:01:30 +0200

    Moves Post.vue, adds PostModal.vue

---
+++ frontend/src/components/homepage/Post.vue
@@ -0,0 +1,42 @@
+<template>
+  <div @click="showModal = true">
+    <div class="float-right">{{formattedTime}}</div>
+    <br>
+    <div class="text-justify text-center"> {{ body }}</div>
+  </div>
+  <Teleport to="body">
+    <postModal :show="showModal" @close="showModal = false">
+      <template #time>
+        {{formattedTime}}
+      </template>
+      <template #body>
+        {{body}}
+      </template>
+    </postModal>
+  </Teleport>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+import PostModal from "./modals/PostModal.vue"
+
+const props = defineProps(
+    {
+      id: String,
+      time: String,
+      body: String
+    }
+);
+
+const showModal = ref(false);
+const formattedTime = ref('');
+
+const dateTime = new Date(props.time);
+formattedTime.value = dateTime.toLocaleString();
+
+</script>
+
+<style scoped>
+
+</style>
