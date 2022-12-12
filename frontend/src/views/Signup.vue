b6a3cd7b3696eca7f43c157e62b740a01c5bd11e
Robert Matsar <robert.matsar@gmail.com>
2022-12-12 19:58:26 +0200

    mk signup.vue

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,16 @@
+<template>
+    <SignupForm></SignupForm>
+</template>
+  
+
+<script setup>
+import SignupForm from '../components/SignupForm.vue';
+
+  
+
+</script>
+
+
+<style scoped>
+
+</style>
